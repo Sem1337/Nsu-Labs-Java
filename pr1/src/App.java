2710f4eae356da4273f4f9c92ce2dd0aaa913ae0
Semyon <semenvlasov2000@gmail.com>
2020-02-08 18:07:21 +0700

    upd

--- pr1/src/App.java
+++ pr1/src/App.java
@@ -3,7 +3,8 @@ import java.io.IOException;
 public class App {
 
     public static void main(String[] args) throws IOException {
-        FileHandler handler = new FileHandler(args[0]);
+        String inputFileName = args.length > 0 ? args[0] : "input.txt";
+        FileHandler handler = new FileHandler(inputFileName);
         CSVWriter writer = new CSVWriter("out.csv");
         writer.write(handler.getFrequency(), handler.getWordsCount());
     }
